b0a91e60e65b9c1805cf5c57aa85a806b1fc745b
aninditaroy <anindita.roy.mu@gmail.com>
2022-04-29 18:18:59 +0600

    home page items added

--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import banner from '../../../images/perfumes/banner/banner.png';
 import './Home.css';
+import usePerfumes from './../../../hooks/usePerfumes';
+import Perfume from '../Perfume/Perfume';
 const Home = () => {
+    const [perfumes] = usePerfumes();
     return (
         <div>
-            <div class="sm:p-24 bg-gradient-to-r from-rose-100 to-amber-100/30 grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 lg:gap-3 lg:p-0" style={{ height: '750px' }}>
+            <div className="sm:p-24 bg-gradient-to-r from-rose-100 to-amber-100/30 grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 lg:gap-3 lg:p-0" style={{ height: '750px' }}>
                 <img src={banner} alt="" className='lg:px-40 lg:w-full sm:w-80' />
                 <div>
                     <h1 className='lg:px-10 text-5xl lg:text-8xl text-amber-900/70 lg:ml-11 lg:mt-60 font-bold' style={{ paddingLeft: '15px' }}>Best Perfume</h1>
                     <h4 className='lg:px-10 text-xl  text-amber-800 lg:ml-12 font-bold mt-5' style={{ paddingLeft: '15px' }}>For her, for him, for you this is a collection of fragrances coloured by the emotions to discover, love and share.We Distributes fragrances around the world for a variety of fashion brands, celebrities and lifestyles.</h4>
+                </div>
+            </div>
+            <div>
+                <h1 className='lg:px-10 text-5xl lg:text-5xl text-amber-900/70 lg:ml-11 lg:mt-11 lg:mb-10 font-bold text-center'><span className='border-amber-900 
+                             border-b-2'>Our Perfumes</span></h1>
+                <div className="lg:mx-auto xl:mx-auto 2xl:mx-auto md:mx-auto">
+                    <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 lg:gap-10 mb-20 md:gap-3 sm:gap-y-3 lg:px-10 '>
+                     {
+                         perfumes.slice(0,3).map(perfume => <Perfume key={perfume._id} perfume={perfume}/>)
+                     }
+                    </div>
                 </div>
             </div>
         </div>
--- src/components/Home/Perfumes/Perfumes.js
+++ src/components/Home/Perfumes/Perfumes.js
@@ -9,7 +9,7 @@ const Perfumes = () => {
         <>
             <div className="relative flex min-h-screen flex-col justify-center bg-gradient-to-r from-rose-100 to-amber-100/50">
                 <div className="lg:mx-auto xl:mx-auto 2xl:mx-auto md:mx-auto">
-                    <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 lg:gap-10 mb-20 md:gap-3 sm:gap-y-3 lg:px-10'>
+                    <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 lg:gap-16 mb-20 md:gap-3 sm:gap-y-3 lg:px-10'>
                      {
                          perfumes.map(perfume => <Perfume key={perfume._id} perfume={perfume}/>)
                      }
