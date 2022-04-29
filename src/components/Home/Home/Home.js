dad8b4285b2c3e01071d8ba4ac584895051b0fbe
aninditaroy <anindita.roy.mu@gmail.com>
2022-04-29 18:04:14 +0600

    home page banner modified

--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import banner from '../../../images/perfumes/banner/banner-1.png';
+import banner from '../../../images/perfumes/banner/banner.png';
 import './Home.css';
 const Home = () => {
     return (
@@ -8,7 +8,7 @@ const Home = () => {
                 <img src={banner} alt="" className='lg:px-40 lg:w-full sm:w-80' />
                 <div>
                     <h1 className='lg:px-10 text-5xl lg:text-8xl text-amber-900/70 lg:ml-11 lg:mt-60 font-bold' style={{ paddingLeft: '15px' }}>Best Perfume</h1>
-                    <h4 className='lg:px-10 text-xl  text-amber-800 lg:ml-12 font-bold mt-5' style={{ paddingLeft: '15px' }}>For her, for him, for you, this is a collection of fragrances coloured by the emotions to discover, love and share.We Distributes fragrances around the world for a variety of fashion brands, celebrities and lifestyles.</h4>
+                    <h4 className='lg:px-10 text-xl  text-amber-800 lg:ml-12 font-bold mt-5' style={{ paddingLeft: '15px' }}>For her, for him, for you this is a collection of fragrances coloured by the emotions to discover, love and share.We Distributes fragrances around the world for a variety of fashion brands, celebrities and lifestyles.</h4>
                 </div>
             </div>
         </div>
